aab317ee1ca8a815ccb122849fd900d0223d8dd8
Marat Gubaidullin <marat.gubaidullin@gmail.com>
2021-07-28 11:51:09 -0400

    first commit

---
+++ camel-springboot/src/main/java/org/example/camel/Application.java
@@ -0,0 +1,27 @@
+package org.example.camel;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.stereotype.Component;
+
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Component
+    class RestApi extends RouteBuilder {
+
+        @Override
+        public void configure() {
+            from("timer:demo?period=1s&repeatCount=10")
+                    .setBody(constant("Hello World"))
+                    .log("${body}");
+        }
+    }
+
+}
